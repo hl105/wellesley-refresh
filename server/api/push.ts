a9282c78c1caab23dca3003c5a1e4b8679d35b50
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2025-03-19 15:46:47 -0400

    updates push.ts to match python scraping

--- server/api/push.ts
+++ server/api/push.ts
@@ -1,6 +1,29 @@
 // server/push.ts
 import { createClient } from "@supabase/supabase-js";
 
+const ALLERGENS_AND_PREFERENCE_FIELDS = [
+  "has_dairy",
+  "has_egg",
+  "has_fish",
+  "has_peanut",
+  "has_sesame",
+  "has_soy",
+  "has_tree_nut",
+  "has_wheat",
+  "has_shellfish",
+  "may_contain_dairy",
+  "may_contain_egg",
+  "may_contain_peanut",
+  "may_contain_sesame",
+  "may_contain_soy",
+  "may_contain_tree_nut",
+  "may_contain_wheat",
+  "is_vegetarian",
+  "is_vegan",
+  "is_gluten_sensitive",
+  "is_nutrigood",
+]
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
 
@@ -10,9 +33,22 @@ export default defineEventHandler(async (event) => {
 
   const supabase = createClient(body.supabaseUrl, body.supabaseKey);
 
+  let payload = {
+    date: body.date,
+    dhall: body.dhall,
+    meal: body.meal,
+    name: body.name,
+    description: body.description,
+    station: body.station,
+    stationOrder: body.stationOrder,
+  };
+  ALLERGENS_AND_PREFERENCE_FIELDS.forEach(field => {
+    if (field in body) {payload[field] = true;}
+  });
+
   const { data, error } = await supabase
     .from('Menu')
-    .insert([{ dhall: body.dhall }])
+    .insert([payload])
     .select();
 
   if (error) {
