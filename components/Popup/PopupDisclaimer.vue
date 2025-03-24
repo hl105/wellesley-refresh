72d6bdaf6fb308e7bafd0523cbd7a94b1260dc29
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2025-03-24 14:15:23 -0400

    Update disclaimer wording

--- components/Popup/PopupDisclaimer.vue
+++ components/Popup/PopupDisclaimer.vue
@@ -6,7 +6,7 @@ const emit = defineEmits(['close'])
     <div class="popup-overlay">
         <div class="popup-content">
             <h2 class="disclaimer-text">Disclaimer</h2>
-            <p class="disclaimer-text">This is the *exact* same menu on the Wellesley Fresh website. If the menu is incorrect here, it is because it is also incorrect there!</p>
+            <p class="disclaimer-text">This menu has the <em>same</em> information as on the Wellesley Fresh website, just presented in a nicer format. If the menu is incorrect here, it is because it is also incorrect there!</p>
             <button class="close-button" @click="emit('close')">close</button>
         </div>
     </div>
